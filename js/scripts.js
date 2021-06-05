ff01c94798d8d02ad1f77bb69ed9abd9fa31e6fd
Scott Hutley <scotthutley1@comcast.net>
2021-06-05 13:58:04 -0700

    start (document) js code for survey questions WIP

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    event.preventDefault();
+    const question1 = $("input#question1").val();
+    const question2 = $("input#question2").val();
+    const question3 = $("input#question3").val();
+    const question4 = $("input#question4").val();
+    const question5 = $("input#question5").val();
+
+    $(".question1").text(question1Input);
+    $(".question2").text(question2Input);
+    $(".question3").text(question3Input);
+    $(".question4").text(question4Input);
+    $(".question5").text(question5Input);
+
+    $("#result").show();
+  });
+});
